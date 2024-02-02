16968b17daea9431cb815a07908adf8a09b59d54
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-02-02 15:54:10 -0500

    restructerd thoughtRoutes

--- Develop/routes/api/thoughtRoutes.js
+++ Develop/routes/api/thoughtRoutes.js
@@ -1,119 +1,5 @@
-const Thought = require("../models/Thought");
-
-const getAllThoughts = async (req, res) => {
-  try {
-    const thoughts = await Thought.find();
-    res.json(thoughts);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-};
-
-const getSingleThought = async (req, res) => {
-  try {
-    const thought = await Thought.findById(req.params.id).populate("reactions");
-
-    if (!thought) {
-      return res.status(404).json({ message: "Thought not found" });
-    }
-
-    res.json(thought);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-};
-
-const createThought = async (req, res) => {
-  try {
-    const newThought = await Thought.create(req.body);
-
-    // Push the created thought's _id to the associated user's thoughts array field
-    const userId = req.body.userId; // Assuming you include the userId in the request body
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    user.thoughts.push(newThought._id);
-    await user.save();
-
-    res.status(201).json(newThought);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
-
-const updateThought = async (req, res) => {
-  try {
-    const updatedThought = await Thought.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-
-    if (!updatedThought) {
-      return res.status(404).json({ message: "Thought not found" });
-    }
-
-    res.json(updatedThought);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
-
-const deleteThought = async (req, res) => {
-  try {
-    const deletedThought = await Thought.findByIdAndDelete(req.params.id);
-
-    if (!deletedThought) {
-      return res.status(404).json({ message: "Thought not found" });
-    }
-
-    res.json({ message: "Thought deleted" });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-};
-
-const createReaction = async (req, res) => {
-  try {
-    const thought = await Thought.findById(req.params.thoughtId);
-
-    if (!thought) {
-      return res.status(404).json({ message: "Thought not found" });
-    }
-
-    thought.reactions.push(req.body);
-    await thought.save();
-
-    res.status(201).json(thought);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
-
-const deleteReaction = async (req, res) => {
-  try {
-    const thought = await Thought.findById(req.params.thoughtId);
-
-    if (!thought) {
-      return res.status(404).json({ message: "Thought not found" });
-    }
-
-    const reactionId = req.body.reactionId;
-    thought.reactions = thought.reactions.filter(
-      (reaction) => reaction.reactionId.toString() !== reactionId
-    );
-    await thought.save();
-
-    res.json(thought);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-};
-
-module.exports = {
+const router = require('express').Router();
+const {
   getAllThoughts,
   getSingleThought,
   createThought,
@@ -121,4 +7,54 @@ module.exports = {
   deleteThought,
   createReaction,
   deleteReaction,
-};
+} = require('../controllers/thoughtController');
+
+router.route('/').get(async (req, res) => {
+  try {
+    await getAllThoughts(req, res);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}).post(async (req, res) => {
+  try {
+    await createThought(req, res);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.route('/:id').get(async (req, res) => {
+  try {
+    await getSingleThought(req, res);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}).put(async (req, res) => {
+  try {
+    await updateThought(req, res);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}).delete(async (req, res) => {
+  try {
+    await deleteThought(req, res);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.route('/:thoughtId/reactions').post(async (req, res) => {
+  try {
+    await createReaction(req, res);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}).delete(async (req, res) => {
+  try {
+    await deleteReaction(req, res);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
