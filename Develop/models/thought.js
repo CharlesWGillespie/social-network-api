179001e3a17b00fb8fe310af8a7be72644e32624
CharlesWGillespie <charlesgillespiew@gmail.com>
2024-02-02 14:56:30 -0500

    created model for thought schema in thought.js

---
+++ Develop/models/thought.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => 
+        new Date(timestamp).toISOString(),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ReactionSchema],
+});
